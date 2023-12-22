8b7e195e0c3878374860c239f7ba82a28c29abb8
pine-box <robert.milroy@outlook.com>
2023-12-22 20:43:51 +0000

    added timer and end screen logic

---
+++ assets/js/logic.js
@@ -0,0 +1,43 @@
+const timerElement = document.querySelector('.timer')
+const startButton = document.querySelector("#startQuiz-Button");
+const start = document.querySelector("#start_screen_container");
+const end = document.querySelector("#end-screen")
+
+let timer;
+let timerCount = 0;
+
+
+// The setTimer function starts and stops the timer
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            showFinalScore();
+        }
+    }, 1000);
+}
+
+function showFinalScore() {
+    end.style.display = "block";
+    start.style.display = "none";
+    startButton.disabled = false;
+}
+
+
+// The startGame function is called when the start button is clicked
+function startQuiz() {
+    timerCount = 2;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    startTimer()
+}
+
+
+// Attach event listener to start button to call startQuiz function on click
+startButton.addEventListener("click", startQuiz);
+
