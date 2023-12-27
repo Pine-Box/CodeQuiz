e7c517972baa4e165688469dab691889552af7cf
pine-box <robert.milroy@outlook.com>
2023-12-27 21:57:09 +0000

    toggling start and submit buttons

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,23 @@
 const timerElement = document.querySelector('.timer')
-const startButton = document.querySelector("#startQuiz-Button");
-const start = document.querySelector("#start_screen_container");
-const end = document.querySelector("#end-screen")
+const startButton = document.getElementById("startQuiz-Button");
+const submitButton = document.getElementById("submit-Button");
+const start = document.getElementById("start-screen");
+const end = document.getElementById("end-screen")
 
 let timer;
 let timerCount = 0;
+let stoptimer = false;
+
 
 
 // The setTimer function starts and stops the timer
 function startTimer() {
     // Sets timer
     timer = setInterval(function () {
-        timerCount--;
-        timerElement.textContent = timerCount;
+        if  (stoptimer === false) {
+            timerCount--;
+            timerElement.textContent = timerCount;
+        }
         // Tests if time has run out
         if (timerCount === 0) {
             // Clears interval
@@ -22,24 +27,30 @@ function startTimer() {
     }, 1000);
 }
 
+function init() {
+    end.style.display = "none";
+    start.style.display = "block";
+    startButton.style.display = "block";
+    submitButton.style.display = "none";
+    stoptimer = false;
+    timerCount = 5;
+}
+
 function showFinalScore() {
     end.style.display = "block";
     start.style.display = "none";
-    startButton.disabled = false;
+    startButton.style.display = "none";
+    submitButton.style.display = "block";
 }
 
 
 // The startGame function is called when the start button is clicked
 function startQuiz() {
-    timerCount = 5;
-    // Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
-        end.style.display = "none";
-    start.style.display = "block";
     startTimer()
 }
 
-
+init();
 // Attach event listener to start button to call startQuiz function on click
 startButton.addEventListener("click", startQuiz);
+submitButton.addEventListener("click", init);
 
